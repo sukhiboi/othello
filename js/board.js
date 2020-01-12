64e59828c80ce6722397bf303b5de549dbc5b6c1
sukhiboi <41sukhdevsingh@gmail.com>
2020-01-12 17:19:31 +0530

    hard coded initial coin in the game itself

--- js/board.js
+++ js/board.js
@@ -3,7 +3,12 @@ class Game {
     this.player1 = player1;
     this.player2 = player2;
     this.turn = player1;
-    this._coins = [...initialCoins];
+    this._coins = [
+      { id: "3_3", color: "black" },
+      { id: "3_4", color: "white" },
+      { id: "4_3", color: "white" },
+      { id: "4_4", color: "black" }
+    ];
   }
 
   coins() {
@@ -21,17 +26,17 @@ class Game {
   }
 }
 
-const setupGame = function () {
-  const createCell = function (cellId) {
-    const cell = document.createElement('div');
+const setupGame = function() {
+  const createCell = function(cellId) {
+    const cell = document.createElement("div");
     cell.id = cellId;
-    cell.className = 'cell selectable';
+    cell.className = "cell selectable";
     cell.innerText = cellId;
     return cell;
   };
 
-  const createBoard = function (numOfRows, numOfCols) {
-    const board = document.getElementById('board');
+  const createBoard = function(numOfRows, numOfCols) {
+    const board = document.getElementById("board");
     for (let rowId = 0; rowId < numOfRows; rowId++) {
       for (let colId = 0; colId < numOfCols; colId++) {
         const cell = createCell(`${rowId}_${colId}`);
@@ -40,43 +45,32 @@ const setupGame = function () {
     }
   };
 
-  const numOfRows = 8, numOfCols = 8;
+  const numOfRows = 8,
+    numOfCols = 8;
   createBoard(numOfRows, numOfCols);
 };
 
-const displayCoins = function (coins) {
+const displayCoins = function(coins) {
   coins.forEach(coin => {
     const cell = document.getElementById(coin.id);
-    const currentCoin = document.createElement('div');
+    const currentCoin = document.createElement("div");
     currentCoin.className = `coin ${coin.color}`;
     cell.appendChild(currentCoin);
-  })
+  });
 };
 
-const addClickListener = function (game) {
-  const board = document.getElementById('board');
-  board.addEventListener('click', () => {
+const addClickListener = function(game) {
+  const board = document.getElementById("board");
+  board.addEventListener("click", () => {
     game.addCoinAt(event.target.id);
     displayCoins(game.coins());
-  })
-};
-
-const createInitialCoins = function () {
-  const createCoin = (id, color) => { return { id, color } };
-  const coinsInfo = [['3_3', 'black'], ['3_4', 'white'], ['4_3', 'white'], ['4_4', 'black']];
-  const initialCoins = coinsInfo.map(info => {
-    const [id, color] = info;
-    return createCoin(id, color);
   });
-  return initialCoins;
 };
 
-const main = function () {
-  const player1 = { name: 'alpha', color: 'white' };
-  const player2 = { name: 'beta', color: 'black' };
+const main = function() {
+  const player1 = { name: "alpha", color: "white" };
+  const player2 = { name: "beta", color: "black" };
   setupGame();
-  const initialCoins = createInitialCoins();
-  const game = new Game(player1, player2, initialCoins);
-  addClickListener(game, board);
-  displayCoins(initialCoins);
+  const game = new Game(player1, player2);
+  addClickListener(game);
 };
